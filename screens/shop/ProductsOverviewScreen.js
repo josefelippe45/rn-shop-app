2d156e16483cc1e63c2461ac84eee0f59d673f51
José Felippe Gonçalves Oliveira <jose.oliveira370@etec.sp.gov.br>
2020-08-24 22:05:38 -0300

    product design

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,13 +1,40 @@
 /**Screen the user see when the app loads */
-import react from 'react';
-import { StyleSheet, FlatList } from 'react-native';
+import React from 'react';
+import { FlatList } from 'react-native';
+/**will allow to tap into redux store and get the products from there */
+import { useSelector } from 'react-redux';
+
+import ProductItem from '../../components/shop/ProductItem';
 
 const ProductsOverviewScreen = props => {
-    return <FlatList/>
-}
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    }
-});
+    //useSelector receives the state as an input and return the data i want
+    const products = useSelector(state => state.products.availableProducts);
+    return (
+        <FlatList
+
+            data={products}
+            //itemData has the item prop and each item is an item on products
+            renderItem={itemData => (
+                <ProductItem
+                    image={itemData.item.imageUrl}
+                    title={itemData.item.title}
+                    price={itemData.item.price}
+                    onViewDetail={() => { }}
+                    onAddToCart={() => { }}
+                />
+            )}
+        />
+    );
+};
+
+ProductsOverviewScreen.navigationOptions = {
+    headerTitle: 'All Products'
+};
+
 export default ProductsOverviewScreen;
+
+
+
+
+
+
